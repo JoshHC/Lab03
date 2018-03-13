6c31334afe127ae4c432df52bdf8c300dba28e7d
paas10 <pablo.andre_alvarado@hotmail.com>
2018-03-12 21:54:54 -0600

    AVL

---
+++ Lab03/Libreria de Clases/IArbolAVL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TDALibrary;
+
+namespace TercerProyecto
+{
+    public interface IArbolAVL<T, K>: IArbolBusquedaBinario<T, K>
+    {
+
+    }
+}
